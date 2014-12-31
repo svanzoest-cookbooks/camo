c7da475ff05107da2e1fc89b31db9d3359b50a80
armiller <armiller@osuosl.org>
2014-12-31 15:01:07 -0800

    Bump cookbook locally

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@onehealth.com'
 license 'Apache 2.0'
 description 'Configures camo - a small http proxy to simplify routing images through an SSL host'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0'
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.5'
 depends	'nodejs'

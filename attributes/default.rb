1ddefd18b69324d666d110ed040b1eb30fa6e1a0
Sander van Zoest <sander@vanzoest.com>
2016-10-30 18:10:32 -0700

    fix rubocop lint errors

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,12 +9,12 @@ default['camo']['path'] = '/srv/camo'
 default['camo']['user'] = 'camo'
 default['camo']['group'] = 'users'
 default['camo']['install_method'] = 'deploy_revision'
-default['camo']['systemd']['env_path'] =  value_for_platform(
+default['camo']['systemd']['env_path'] = value_for_platform(
   %w(ubuntu debian) => {
-     'default' => '/etc/default'
+    'default' => '/etc/default'
   },
   %w(centos rhel) => {
-     'default' => '/etc/sysconfig'
+    'default' => '/etc/sysconfig'
   }
 )
 default['camo']['init_style'] = value_for_platform(
